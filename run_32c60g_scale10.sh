b570015947e420ac152ac1933c2b6362caceb9b8
gabrielranulfo <gabrielranulfo14@gmail.com>
2026-01-06 22:13:00 -0300

    atualizado o compose e os runs individuais

--- run_32c60g_scale10.sh
+++ run_32c60g_scale10.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-#SBATCH --job-name=polars_benchmark_16c64g_scale10
+#SBATCH --job-name=polars_benchmark_32c60g_scale10
 #SBATCH --partition=draco
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=60G
 #SBATCH --time=10:00:00
-#SBATCH --output=polars_benchmark_16c64g_scale10_%x_%j.out
-#SBATCH --error=polars_benchmark_16c64g_scale10_%x_%j.err
-ulimit -v $((64 * 1024 * 1024))
+#SBATCH --output=polars_benchmark_32c60g_scale10_%x_%j.out
+#SBATCH --error=polars_benchmark_32c60g_scale10_%x_%j.err
+ulimit -v $((60 * 1024 * 1024))
 
 echo "Slurm CPUs: $SLURM_CPUS_PER_TASK"
 echo "Slurm Memória: $SLURM_MEM_PER_NODE"
 
 export escala=10.0
 export rep=50
+export imagem="polars-benchmark-32c60g"
 
 for ((i=1; i<=rep; i++))
 do
     echo "Execução $i/$rep"
 
     docker compose run --rm \
-      --cpus 16 \
-      --memory 64g \
+      --cpuset-cpus 0-31 \
+      --memory 60g \
       -e SCALE_FACTOR=$escala \
-      polars-benchmark \
+      $imagem \
       bash -c "cd /root/polars-benchmark && ./run.sh"
 
     if [ $i -lt $rep ]; then
--- run_8c60g_scale01.sh
+++ run_8c60g_scale01.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-#SBATCH --job-name=polars_benchmark_32c64g_scale100
+#SBATCH --job-name=polars_benchmark_8c60g_scale01
 #SBATCH --partition=draco
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=60G
 #SBATCH --time=10:00:00
-#SBATCH --output=polars_benchmark_32c64g_scale100_%x_%j.out
-#SBATCH --error=polars_benchmark_32c64g_scale100_%x_%j.err
-ulimit -v $((64 * 1024 * 1024))
+#SBATCH --output=polars_benchmark_8c60g_scale01_%x_%j.out
+#SBATCH --error=polars_benchmark_8c60g_scale01_%x_%j.err
+ulimit -v $((60 * 1024 * 1024))
 
 echo "Slurm CPUs: $SLURM_CPUS_PER_TASK"
 echo "Slurm Memória: $SLURM_MEM_PER_NODE"
 
-export escala=100.0
+export escala=1.0
 export rep=50
+export imagem="polars-benchmark-8c60g"
 
 for ((i=1; i<=rep; i++))
 do
     echo "Execução $i/$rep"
 
     docker compose run --rm \
-      --cpus 32 \
-      --memory 64g \
+      --cpuset-cpus 0-7 \
+      --memory 60g \
       -e SCALE_FACTOR=$escala \
-      polars-benchmark \
+      $imagem \
       bash -c "cd /root/polars-benchmark && ./run.sh"
 
     if [ $i -lt $rep ]; then
