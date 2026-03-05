5b52602f79b2d36aefe7ec2cf7bee432809d5f12
gabrielranulfo <gabrielranulfo14@gmail.com>
2026-03-04 21:53:07 -0300

    adcionado os slurms para 1 core de cpu

---
+++ run_1c60g_scale01.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH --job-name=polars_benchmark_1c60g_scale01
+#SBATCH --partition=draco
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=60G
+#SBATCH --time=24:00:00
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+ulimit -v $((60 * 1024 * 1024))
+
+echo "Slurm CPUs: $SLURM_CPUS_PER_TASK"
+echo "Slurm Memória: $SLURM_MEM_PER_NODE"
+
+export escala=1.0
+export rep=50
+export imagem="polars-benchmark-1c60g"
+
+export POLARS_MAX_THREADS=1
+export DASK_NUM_WORKERS=1
+export DASK_THREADS_PER_WORKER=1
+export OMP_NUM_THREADS=1  
+export MKL_NUM_THREADS=1
+export DUCKDB_THREADS=1
+export DUCKDB_EXTERNAL_THREADS=1
+export DUCKDB_ASYNC_IO_THREADS=1
+export SPARK_EXECUTOR_CORES=1
+export SPARK_EXECUTOR_INSTANCES=1
+export SPARK_DRIVER_CORES=1
+export SPARK_DEFAULT_PARALLELISM=8  # 1 core * 8 partitions per core
+export SPARK_SQL_SHUFFLE_PARTITIONS=8
+
+
+for ((i=1; i<=rep; i++))
+do
+    echo "Execução $i/$rep"
+
+    docker compose run --rm \
+      -e SCALE_FACTOR=$escala \
+      -e POLARS_MAX_THREADS=$POLARS_MAX_THREADS \
+      -e OMP_NUM_THREADS=$OMP_NUM_THREADS \
+      -e MKL_NUM_THREADS=$MKL_NUM_THREADS \
+      -e DASK_NUM_WORKERS=$DASK_NUM_WORKERS \
+      -e DASK_THREADS_PER_WORKER=$DASK_THREADS_PER_WORKER \
+      -e DUCKDB_THREADS=$DUCKDB_THREADS \
+      -e DUCKDB_EXTERNAL_THREADS=$DUCKDB_EXTERNAL_THREADS \
+      -e DUCKDB_ASYNC_IO_THREADS=$DUCKDB_ASYNC_IO_THREADS \
+      -e SPARK_EXECUTOR_CORES=$SPARK_EXECUTOR_CORES \
+      -e SPARK_EXECUTOR_INSTANCES=$SPARK_EXECUTOR_INSTANCES \
+      -e SPARK_DRIVER_CORES=$SPARK_DRIVER_CORES \
+      -e SPARK_DEFAULT_PARALLELISM=$SPARK_DEFAULT_PARALLELISM \
+      -e SPARK_SQL_SHUFFLE_PARTITIONS=$SPARK_SQL_SHUFFLE_PARTITIONS \
+      $imagem \
+      bash -c "cd /root/polars-benchmark && ./run.sh"
+
+    if [ $i -lt $rep ]; then
+        sleep 30
+    fi
+done
